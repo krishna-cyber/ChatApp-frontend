36e9708121ed6021171d77893332bcc1cabe6a40
krishna_tiwari <61178259+krishna-cyber@users.noreply.github.com>
2023-03-25 11:26:52 +0545

    updated client

--- utils/server.js
+++ utils/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://backendchatapp-9q6k.onrender.com",
   withCredentials: true,
 });
 
